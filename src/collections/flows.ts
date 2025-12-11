6c22af93f20d4a7cce8793e3cf2f06504d874852
Ruud Andriessen <ruud.andriessen@gmail.com>
2025-12-11 15:18:59 +0100

    refactor: introduce flow strategies

--- src/collections/flows.ts
+++ src/collections/flows.ts
@@ -4,6 +4,28 @@ import {
 } from "@tanstack/react-db";
 import z from "zod";
 
+const fixedStrategy = z.object({
+	type: z.literal("fixed"),
+	config: z.object({
+		amount: z.number(),
+		// insufficientFundsBehavior: z.enum(["strict", "partial"]),
+	}),
+});
+
+const morgageStrategy = z.object({
+	type: z.literal("mortgage"),
+	config: z.object({
+		liabilityAccountId: z.string(),
+		interestExpenseAccountId: z.string(),
+		assetAccountId: z.string(),
+		totalPaymentAmount: z.number(),
+		interestCalculation: z.object({
+			type: z.literal("FIXED_RATE"),
+			baseAnnualRate: z.number(),
+		}),
+	}),
+});
+
 export const flowSchema = z.object({
 	id: z.string(),
 	name: z.string(),
@@ -11,18 +33,13 @@ export const flowSchema = z.object({
 	// Execution Priority
 	// Lower numbers run first (Income -> Transfers -> Bills -> Savings)
 	priorityOrder: z.number(),
+	schedule: z.string(),
 
 	// Flow direction
 	sourceAccountId: z.string(),
 	targetAccountId: z.string(),
 
-	// Maps to a code class: FixedStrategy, MortgageStrategy, WaterfallStrategy
-	strategyType: z.enum(["fixed"]),
-	strategyConfig: z.object({
-		amount: z.number(),
-		// insufficientFundsBehavior: z.enum(["strict", "partial"]),
-	}),
-
+	strategy: z.discriminatedUnion("type", [fixedStrategy, morgageStrategy]),
 	modifiers: z.array(z.enum(["inflation_adjusted"])),
 });
 
