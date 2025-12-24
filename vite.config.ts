6f55800ad42c807296b0620d6146342766b0d50f
Ruud Andriessen <ruud.andriessen@gmail.com>
2025-12-24 13:39:50 +0100

    build: add typescript checker to vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [tanstackRouter(), react(), tailwindcss()],
+  plugins: [
+    tanstackRouter(),
+    react(),
+    tailwindcss(),
+    checker({
+      typescript: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
